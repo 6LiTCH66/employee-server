5fb7c13c07dcac61712f7471349c4aa4f642e7b5
ilja0303 <ilja200303@gmail.com>
2022-01-13 16:29:07 +0200

    cors was added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express")
+const cors = require('cors')
 const routers = require("./routes/employee.routers")
 const PORT = process.env.PORT || 8080
 const app = express()
-
+app.use(cors());
 app.use(express.json())
 app.use('/api', routers)
-
 
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
