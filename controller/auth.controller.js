5e1fa56d7970ebf31fa546c6936b7bc29a58bb86
ilja0303 <ilja200303@gmail.com>
2022-01-20 11:39:02 +0200

    cookie fix

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -55,7 +55,7 @@ const signin = async (req, res) =>{
                     expiresIn: "15m"
                 }
             );
-            res.cookie("access_token", accessToken, {httpOnly: true, maxAge: 15 * 60 * 1000, overwrite: true, sameSite: "none", secure: true})
+            res.cookie("access_token", accessToken, {httpOnly: true, maxAge: 15 * 60 * 1000, overwrite: true, sameSite: "none", secure: true, domain: "https://employee-client-app.herokuapp.com/"})
             await updateUserAuth(user.id, Date.now(), req.socket.remoteAddress, req.get('User-Agent'), accessToken)
 
             const {password, ...data} = await user.toJSON()
@@ -85,7 +85,7 @@ const token = async (req, res) =>{
             }
         );
 
-        res.cookie("access_token", accessToken, {httpOnly: true, maxAge: 15 * 60 * 1000, overwrite: true, sameSite: "none", secure: true})
+        res.cookie("access_token", accessToken, {httpOnly: true, maxAge: 15 * 60 * 1000, overwrite: true, sameSite: "none", secure: true, domain: "https://employee-client-app.herokuapp.com/"})
         res.status(200).json(user);
 
     }catch (error){
